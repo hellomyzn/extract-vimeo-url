f8e11bc7f3220489c014e0c76e9b3337a270f442
hellomyzn <em711th@icloud.com>
2023-03-02 14:25:50 +0900

    update:

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import json
 
-def extract_vimeo_url(path):
+def extract_vimeo_url(path: str) -> str:
     # retrieve chunks including 720p
-    txt_file = path
     txt_data = []
 
-    with open(txt_file, mode="r") as f:
+    with open(path, mode="r") as f:
         for line in f:
             if "720p" in line:
                 txt_data.append(line)
@@ -13,19 +12,20 @@ def extract_vimeo_url(path):
     # retrieve json data
     target = " = "
     idx = txt_data[0].find(target)
-    json_response = txt_data[0][idx+len(target):]
+    json_data = txt_data[0][idx+len(target):]
 
     # retrieve url from json data
-    json_load = json.loads(json_response)
+    json_load = json.loads(json_data)
     videos = json_load['request']['files']['progressive']
 
     for v in videos:
         if v['quality'] == '720p':
-            print(v['url'])
-
+            video_url_720 = v['url']
     
+    return video_url_720
+
 
 if __name__ == '__main__':
     import sys
     args = sys.argv
-    extract_vimeo_url(args[1])
+    print(extract_vimeo_url(args[1]))
